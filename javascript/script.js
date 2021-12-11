ea818b49b94b4d3509242d51c1f8c0f74c55d9bd
Anthony Carbajal <acehb95@gmail.com>
2021-12-11 13:04:02 -0800

    Pseudoscript in script.js

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+//Create constant guesses with list rock, paper, scissors
+//Check to see if rounds is less than 5 or if the game is over
+//Create variable compGuess that equals randomly chosen item from guesses
+//Prompt user to choose one of rock, paper, scissors and store in userGuess
+//Change userGuess to lower case.
+//Check if userGuess equals rock, paper, or scissors otherwise don't allow to continue
+//Compare userGuess and compGuess
+//Rock beats scissors, scissors beat paper, paper beats rock. 
+//Ties prompt user to choose new userGuess, and compGuess choses new random value.
+//Declare a winner. Add 1 to the compWins or userWins depending on who wins.
+//Add 1 to the total round tally.
+//Repeat until 5 rounds have been played.
+
