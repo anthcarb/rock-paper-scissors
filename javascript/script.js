1f09a58567b149566433189dec48f3b6b4be728b
Anthony Carbajal <acehb95@gmail.com>
2021-12-11 13:47:12 -0800

    Changed variable name to select

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
-//Create constant guesses with list rock, paper, scissors
+//Create constant selects with list rock, paper, scissors
 //Check to see if rounds is less than 5 or if the game is over
-//Create variable compGuess that equals randomly chosen item from guesses
-//Prompt user to choose one of rock, paper, scissors and store in userGuess
-//Change userGuess to lower case.
-//Check if userGuess equals rock, paper, or scissors otherwise don't allow to continue
-//Compare userGuess and compGuess
+//Create variable compSelect that equals randomly chosen item from selects
+//Prompt user to choose one of rock, paper, scissors and store in usersSelect
+//Change userSelect to lower case.
+//Check if userSelect equals rock, paper, or scissors otherwise don't allow to continue
+//Compare userSelect and compselect
 //Rock beats scissors, scissors beat paper, paper beats rock. 
-//Ties prompt user to choose new userGuess, and compGuess choses new random value.
+//Ties prompt user to choose new userSelect, and compSelect choses new random value.
 //Declare a winner. Add 1 to the compWins or userWins depending on who wins.
 //Add 1 to the total round tally.
 //Repeat until 5 rounds have been played.
 
-const guess = [ 'rock', 'paper', 'scissors'];
+const select = [ 'rock', 'paper', 'scissors'];
 let round = 1;
 
 function computerPlay(){
-    return guess[Math.floor(Math.random() * guess.length)];
+    return select[Math.floor(Math.random() * select.length)];
 }
+let compSelect = computerPlay();
 
-let compGuess = computerPlay();
-console.log(compGuess);
+console.log(compSelect);
