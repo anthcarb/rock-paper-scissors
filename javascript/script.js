1f8ae3fc15f51a591f268fead13a46d9db83a8fe
Anthony Carbajal <acehb95@gmail.com>
2022-01-27 17:07:27 -0800

    added funcitonality and cleaned classes/id

--- javascript/script.js
+++ javascript/script.js
@@ -2,18 +2,27 @@ const select = [ 'rock', 'paper', 'scissors'];
 let userWins = 0;
 let compWins = 0;
 
+
 function computerPlay(){
     return select[Math.floor(Math.random() * select.length)];
 }
 // Randomly selects rock, paper, scissors for computer
 
-function userPlay(){
-    // const userInput = document.querySelector('button');
-    // userInput.addEventListener('click', () => console.log('hi'))
-    let playerSelection = 'rock'
-    return playerSelection;
+//this is a scope issue i can't figure out
+function buttonPlay(input) {
+    input.addEventListener('click', (e) => {
+        let playerSelection = e.target.id;
+        const computerSelection = computerPlay();
+        const results = playRound(playerSelection, computerSelection);        
+        displayScore(userWins, compWins);
+        displayResults(results);
+    })
 }
-//Asks user input for rock, paper, or scissors.
+
+function userPlay() {
+    let inputs = document.querySelectorAll('button');
+    inputs.forEach((input) => buttonPlay(input));
+}
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
@@ -49,59 +58,31 @@ function displayResults(results) {
 }
 
 function game() {
+    userPlay();
     while(userWins < 5 && compWins < 5) {
-        const computerSelection = computerPlay();
-        const playerSelection = userPlay();
-        const results = playRound(playerSelection, computerSelection);
-        displayScore(userWins, compWins);
-        displayResults(results);
+        return
     }
     if (userWins > compWins) {
-        results = `Player wins after 5 rounds! Final score is Player: ${userWins} - Computer: ${compWins}` ;
+        results = `Player wins best out of 5! Final score is Player: ${userWins} - Computer: ${compWins}` ;
         displayResults(results);
+        reset();
     } else {
-        results = (`Computer wins after 5 rounds! Final score is Player: ${userWins} - Computer: ${compWins}`);
+        results = (`Computer wins best out of 5! Final score is Player: ${userWins} - Computer: ${compWins}`);
         displayResults(results);
+        reset();
         }
 }
 
-game();
+function reset() {
+    let inputs = document.querySelectorAll('button')
+    const reset = inputs.forEach( (input) =>
+    input.addEventListener('click', () =>{
+        results = '';
+        userWins = 0;
+        compWins = 0;
+    }))
+    console.log("reset performed")
+    return reset;
+}
 
-
-
-// console.log(`Round ${round}. ` + playRound(playerSelection, computerSelection))
-
-
-//THE FOLLOWING IS COMMENTS/CODE NOT USED.
-// Prompts userInput of rock, paper, or scissors. converts input to lower case and checks to see if it's contained in select array.
-// If userInput is not rock paper or scissors, then it informs the user and forces the prompt again until an input that matches one of the selections.
-// Need to fix this somehow. the return of userPlay()
-
-
-// function tieCheck(playerSelection, computerSelection) {
-//     if (playerSelection === computerSelection) {
-//         alert(`TIE! You have both selected ${computerSelection}. Go again!`);    
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// function resetRound(){
-//     userPlay();
-//     computerPlay();
-// }
-
-
-// while (tieCheck(playerSelection, computerSelection) === true) {
-//     userPlay();
-//     computerPlay();
-//     tieCheck(playerSelection, computerSelection);
-// } 
-
-// playRound(playerSelection, computerSelection);
-
-// tie = tieCheck(playerSelection, computerSelection);
-// tie ? resetRound() : playRound(playerSelection, computerSelection);
-
-
+document.querySelector('.playerInput').addEventListener('click', game());
